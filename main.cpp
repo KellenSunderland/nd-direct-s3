040b869ad59075428ceb58727f165685ad06ac9c
Kellen Sunderland <kellen.sunderland@gmail.com>
2019-11-17 06:08:58 +0000

    Applied clang format

--- main.cpp
+++ main.cpp
@@ -9,50 +9,50 @@ std::atomic<int> thread_ids;
 std::atomic<int> files_downloaded;
 
 void download_loop() {
-    int thread_id = thread_ids.fetch_add(1);
-    std::cout << "Download thread " << thread_id << " started" << std::endl;
-    const Aws::String bucket_name = "kellens-alpha-test";
-    const Aws::String object_name = "random-tensors/0.ndarray";
-    const char * filename = "/dev/null";
-    std::ofstream output_file(filename, std::ios::binary);
-    Aws::Client::ClientConfiguration clientConfig;
-    clientConfig.region = "us-west-2";
-    Aws::S3::S3Client s3_client(clientConfig);
-    Aws::S3::Model::GetObjectRequest object_request;
-    object_request.SetBucket(bucket_name);
-    object_request.SetKey(object_name);
-    for(int i =0; i < DOWNLOAD_ITERATIONS; i++) {
-        std::cout << "THREAD: " << thread_id << " downloading new file" << std::endl;
-        auto get_object_outcome = s3_client.GetObject(object_request);
-        if (get_object_outcome.IsSuccess())
-        {
-            auto &retrieved_file = get_object_outcome.GetResultWithOwnership().GetBody();
-            output_file << retrieved_file.rdbuf();
-            files_downloaded.fetch_add(1);
-            std::cout << "THREAD: " << thread_id << " download finished" << std::endl;
-        }
-        else
-        {
-            auto error = get_object_outcome.GetError();
-            std::cout << "THREAD: " << thread_id << " ERROR: " << error.GetExceptionName() << ": " 
+  int thread_id = thread_ids.fetch_add(1);
+  std::cout << "Download thread " << thread_id << " started" << std::endl;
+  const Aws::String bucket_name = "kellens-alpha-test";
+  const Aws::String object_name = "random-tensors/0.ndarray";
+  const char *filename = "/dev/null";
+  std::ofstream output_file(filename, std::ios::binary);
+  Aws::Client::ClientConfiguration clientConfig;
+  clientConfig.region = "us-west-2";
+  Aws::S3::S3Client s3_client(clientConfig);
+  Aws::S3::Model::GetObjectRequest object_request;
+  object_request.SetBucket(bucket_name);
+  object_request.SetKey(object_name);
+  for (int i = 0; i < DOWNLOAD_ITERATIONS; i++) {
+    std::cout << "THREAD: " << thread_id << " downloading new file"
+              << std::endl;
+    auto get_object_outcome = s3_client.GetObject(object_request);
+    if (get_object_outcome.IsSuccess()) {
+      auto &retrieved_file =
+          get_object_outcome.GetResultWithOwnership().GetBody();
+      output_file << retrieved_file.rdbuf();
+      files_downloaded.fetch_add(1);
+      std::cout << "THREAD: " << thread_id << " download finished" << std::endl;
+    } else {
+      auto error = get_object_outcome.GetError();
+      std::cout << "THREAD: " << thread_id
+                << " ERROR: " << error.GetExceptionName() << ": "
                 << error.GetMessage() << std::endl;
-        }
     }
+  }
 }
 
-int main(int argc, char** argv)
-{
-    Aws::SDKOptions options;
-    options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Info;
-    Aws::InitAPI(options);
-    {
-        std::vector<std::thread> download_threads;
-        for (int i = 0; i < NUM_THREADS; i++) {
-            download_threads.push_back(std::thread(download_loop));
-        }
-
-        std::for_each(download_threads.begin(),download_threads.end(),[](auto& t) { t.join(); });
-        std::cout << "Total downloaded files: " << files_downloaded << std::endl;
+int main(int argc, char **argv) {
+  Aws::SDKOptions options;
+  options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Info;
+  Aws::InitAPI(options);
+  {
+    std::vector<std::thread> download_threads;
+    for (int i = 0; i < NUM_THREADS; i++) {
+      download_threads.push_back(std::thread(download_loop));
     }
-    Aws::ShutdownAPI(options);
+
+    std::for_each(download_threads.begin(), download_threads.end(),
+                  [](auto &t) { t.join(); });
+    std::cout << "Total downloaded files: " << files_downloaded << std::endl;
+  }
+  Aws::ShutdownAPI(options);
 }
