d55fcaa97d3a1ce8ebab87786be06df5a7c7ffc7
Kellen Sunderland <kellen.sunderland@gmail.com>
2019-11-24 07:03:54 +0000

    Simplify format tool

--- check_format.sh
+++ check_format.sh
@@ -2,8 +2,4 @@
 
 # Run clang-format linting
 cd /workspaces/nd-direct-s3/
-clang-format-7 -i *.cpp
-
-# Run clang-tidy static code analysis
-cd /workspaces/nd-direct-s3/build
-run-clang-tidy-7.py
+clang-format-7 -i *.cpp *.h
