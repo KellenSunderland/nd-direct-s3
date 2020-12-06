0e258bd4283231f774063f941f22cf8505571e16
Kellen Sunderland <kellen.sunderland@gmail.com>
2020-12-05 17:50:06 -0800

    Fix python logging.

--- tensorizer/python/tensorizer.py
+++ tensorizer/python/tensorizer.py
@@ -3,6 +3,8 @@ import logging
 
 from functools import lru_cache
 
+logging.basicConfig(level=logging.DEBUG)
+
 @lru_cache(maxsize=None)
 def _load_native_tensorizer():
     logging.info("Loading Tensorizer native lib")
