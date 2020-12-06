d63949c1e0dbce58a45384d60cd8ac739c1c6318
Kellen Sunderland <kellen.sunderland@gmail.com>
2020-12-05 17:42:56 -0800

    Small snippet to test library loading.

---
+++ tensorizer/python/tensorizer.py
@@ -0,0 +1,12 @@
+import ctypes
+import logging
+
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def _load_native_tensorizer():
+    logging.info("Loading Tensorizer native lib")
+    return ctypes.cdll.LoadLibrary("libtensorizer.so")
+
+tensorizer = _load_native_tensorizer()
+tensorizer_handle = tensorizer.construct()
